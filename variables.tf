6e82f485d814f3bd785c45c4f8f8b8e297766030
dounkimm <98010656+dounkimm@users.noreply.github.com>
2022-10-11 14:35:41 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "vpc_name" {}
+variable "subnet_name" {}
+
+
+variable "server" {
+    type = map(object({
+        server_name = string
+        login_key_name = string
+        //server_image_product_code = string
+    }))
+}
+
+variable "server_storage" {
+    type = map(object({
+        server_key = string     // server 인스턴스 key
+        storage_name = string
+        disk_type = string      // SSD | HDD
+        disk_size = string      // "10"
+    }))
+}
